fe437c32147da49719648cf36d0ae0249fdce597
fisle <fisle@fisle.eu>
2015-06-24 15:16:46 +0300

    Now playing script

---
+++ nowplaying.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SPOTIFY=$(pidof spotify)
+
+if [ "$SPOTIFY" ]; then
+    SP_STATUS=$(playerctl status)
+    if [ "$SP_STATUS" == "Playing" ]; then
+        ARTIST=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 2 "artist"|egrep -v "artist"|egrep -v "array"|cut -b 27-|cut -d '"' -f 1|egrep -v ^$)
+        TITLE=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'|egrep -A 1 "title"|egrep -v "title"|cut -b 44-|cut -d '"' -f 1|egrep -v ^$)
+        echo -n "np: $ARTIST - $TITLE"
+    fi
+else
+    mpc status | perl -ne 'if (/\[playing\]/) {CORE::say ("",`mpc current|tr -d "\n"`,"")}'
+fi
