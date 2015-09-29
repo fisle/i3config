2a3281dcee7ff44508fae3099b08f0c1ed9e3b5c
Petri Immonen <petri.immonen@scandinavianoutdoor.com>
2015-09-29 09:12:14 +0300

    Right click wan block gets IP address to clipboard

--- external_ip.sh
+++ external_ip.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 IP=$(curl --silent http://afk.fisle.eu/ip.php)
-echo $IP
+
+case $BLOCK_BUTTON in
+    3) echo -n "$IP" | xclip -q -se c ;;
+esac
+
+echo $IP # full text
+echo $IP # short text
